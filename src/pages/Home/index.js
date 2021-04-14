02412e2743122bde58060bd18fbe90cf5d1e1954
caioaugustoo <caioamfr@gmail.com>
2021-04-14 13:32:23 -0300

    Criada lógica para verificar código passado pelo usuário

---
+++ src/pages/Home/index.js
@@ -0,0 +1,52 @@
+import * as S from "./styles";
+
+import Header from "../../components/UI/Header";
+import Input from "../../components/UI/Input";
+import Button from "../../components/UI/Button";
+
+import { Container } from "../../styles/global";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const validSearchOrder = /^[A-Z]{2}\d{9}[A-Z]{2}$/gm;
+
+  function handleSearchOrder(submitEvent) {
+    submitEvent.preventDefault();
+    if (search.trim().length === 0 || !validSearchOrder.test(search)) {
+      setError("Digite um código válido.");
+    } else navigate(`/rastreio/${search}`);
+  }
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <S.Wrapper>
+          <S.Box>
+            <S.Title>Rastrear encomenda</S.Title>
+            <S.Description>
+              Informe o código de rastreio abaixo para rastreamento
+            </S.Description>
+            <form onSubmit={handleSearchOrder}>
+              <Input
+                type="text"
+                value={search}
+                onChange={({ target }) => setSearch(target.value)}
+                placeholder="EX: ON123456789BR"
+                style={{ border: error ? "1px solid red" : "" }}
+              />
+              {!!error && <S.Error>{error}</S.Error>}
+              {!!search && <Button full>Rastrear agora!</Button>}
+            </form>
+          </S.Box>
+        </S.Wrapper>
+      </Container>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 600;
+`;
+
+export const Description = styled.p`
+  font-size: 1.5rem;
+`;
+
+export const Box = styled.div`
+  padding: 30px;
+  background: var(--full-white);
+  max-width: 400px;
+  box-shadow: 0px 0px 50px rgba(0, 0, 0, 0.05);
+  transition: all 0.3s ease;
+  border-radius: 6px;
+
+  &:hover {
+    box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.12);
+  }
+`;
+
+export const Error = styled.p`
+  font-size: 1.3rem;
+  color: red;
+  margin-bottom: 15px;
+`;
